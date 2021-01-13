88ea89a1f23bb0c496f495ae068cdf6f27f96a9d
Quentin ANIERE <quentin.aniere@gmail.com>
2021-01-13 16:49:07 +0100

    ✨ Improve the startup of the app

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
 try {
-    require('electron-reloader')(module)
+    require('electron-reloader')(module);
   } catch (_) {} //For dev purposes only
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 700,
     height: 700,
+    show: false,
+    resizable: false,
+    fullscreen: false,
     webPreferences: {
-      show: false,
-      resizable: false,
-      fullscreen: false,
       nodeIntegration: true
     }
-  })
-  win.setMenuBarVisibility(false)
-  win.loadFile('src/index.html')
+  });
+  win.setMenuBarVisibility(false);
+  win.loadFile('src/index.html');
   win.setIcon('src/img/icon.png');
+  win.once("ready-to-show", function(){
+    win.show()
+  })
 }
 
-app.whenReady().then(createWindow).then(
-  app.once('ready-to-show', () => {
-    win.show()
-  })  
-)
-
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
 })
