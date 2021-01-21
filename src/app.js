40ca4258ae5b651acd6eca0d3f72bcd972c67b11
Quentin ANIERE <quentin.aniere@gmail.com>
2021-01-21 01:50:32 +0100

    💄 Starting to add bottom buttons

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 MicroModal.init();
 
 if(!localStorage.getItem('firstTimeRunningApp')) {
-    //TODO: Uncomment when modal is ready localStorage.setItem("firstTimeRunningApp", false) 
+    localStorage.setItem("firstTimeRunningApp", false) 
     MicroModal.show("modal-welcome");
 }
 
