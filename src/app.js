74276763968e3bab6cca3dc9e927ae79c5262252
qaniere <quentin.aniere@gmail.com>
2021-07-16 22:54:43 +0200

    ✨ Finish the welcome modal

--- src/app.js
+++ src/app.js
@@ -9,15 +9,42 @@ class Title extends React.Component {
 
 class Modal extends React.Component {
 
+    
+    constructor(props) {
+
+        super(props);
+        if(localStorage.getItem(this.props.id) === null) {
+            this.state = {
+                alreadyDisplayed: false,
+            };
+        } else {
+            this.state = JSON.parse(localStorage.getItem(this.props.id));
+        };
+    };
+
     componentDidMount() {
-        MicroModal.show(this.props.id);
+
+        if(this.props.showAtMount && this.state.alreadyDisplayed == false) {
+
+            MicroModal.show(this.props.id);
+            if(this.props.showOnce == true) {
+                this.setState({
+                    alreadyDisplayed: true
+                });
+            };
+        };
     };
 
     close() {
         MicroModal.close(this.props.id);
     };
 
+    componentDidUpdate() {
+        localStorage.setItem(this.props.id, JSON.stringify(this.state));
+    };
+
     render() {
+
         return <div className="modal micromodal-slide" id={this.props.id} aria-hidden="true">
         <div className="modal__overlay" tabIndex="-1" data-micromodal-close>
           <div className="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
@@ -38,11 +65,18 @@ class Modal extends React.Component {
     }; 
 };
 
+Modal.defautProps = {
+    showAtMount: false,
+    showOnce: false
+};
+
 
 ReactDOM.render(<>
     <Title/>
-    <Modal id="welcome-modal" title="Hello There !" buttonText="Got it !">
-        Work in progress...
+    <Modal id="welcome-modal" title="Welcome !" buttonText="Start" showAtMount={true} showOnce={true}>
+        This application allows you to download album covers from Spotify. To start, go 
+        to the spotify web player "open.spotify.com", select an album or a song, paste the url 
+        in the input zone and press download. For further help, click on "configuration" button.
     </Modal>
     </>, 
 document.getElementById("root"));
